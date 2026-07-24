a5c01a30753618caf8a4888962646f56345d5c3d
Sven Fehr <sven.fehr@outlook.de>
2026-07-24 16:42:49 +0200

    fix: adjust health endpoint path and fix swagger ui assets

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
         api: __DIR__.'/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
